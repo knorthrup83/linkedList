07e7066054905801980070bf438879c999e4804a
knorthrup83@gmail.com <knorthrup83@gmail.com>
2019-09-04 17:19:26 +0000

    Added Stack class

--- linkedList.rb
+++ linkedList.rb
@@ -25,6 +25,29 @@ print_values(node3)
 
 
 
+class Stack
+    attr_reader :data
+
+    def initialize
+        @data = nil
+    end
+
+    # Push a value onto the stack
+    def push(value)
+        # IMPLEMENT ME!
+        node = LinkedListNode.New()
+    end
+
+    # Pop an item off the stack.
+    # Remove the last item that was pushed onto the
+    # stack and return the value to the user
+    def pop
+        # I RETURN A VALUE
+    end
+
+end
+
+
 
 
 
